aad42aac263165af5cabb648a0e0cd448addbe5d
Ginga <ginga.timo@gmail.com>
2026-07-19 12:27:31 +0700

    chore(release): v0.5.0 (GDivider, GFab, GGrid, GDatepicker) — 35 component

--- projects/ngx-opendesign/src/public-api.spec.ts
+++ projects/ngx-opendesign/src/public-api.spec.ts
@@ -2,6 +2,6 @@ import { OPENDESIGN_VERSION } from './public-api';
 
 describe('public-api', () => {
   it('should export OPENDESIGN_VERSION', () => {
-    expect(OPENDESIGN_VERSION).toBe('0.4.0');
+    expect(OPENDESIGN_VERSION).toBe('0.5.0');
   });
 });
